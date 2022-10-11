6784dd33d04e2d59859d91c0ed69cf98e8fdd791
samuel ip <samuel.c.ip@student.uts.edu.au>
2022-10-11 20:02:13 +1100

    80% halfway

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -21,5 +22,18 @@ public class UIManager : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.buildIndex == 1)
+        {
+            
+        }
     }
 }
