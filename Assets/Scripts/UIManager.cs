e2f6fa18fad7243889caeabc08d0071ee033901c
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-10-13 03:40:28 +1100

    90% complete

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,16 +6,43 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+   
+    private Transform player;
+    private Image innerBar;
+    private float health;
+    private GameObject canvas;
+    
+    Quaternion originalRoation;
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
+   
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         
+        if (player != null)
+        {
+            health = 1 -  Mathf.Clamp(Mathf.Abs(player.position.x), 0.0f, 5.0f)/5.0f;
+            innerBar.fillAmount = health;
+            if(health > 0.5f && innerBar.color == Color.red)
+            {
+                innerBar.color = new Color32(0, 255, 0, 255);
+            }
+            if (health < 0.5f && innerBar.color == new Color32(0, 255, 0, 255))
+            {
+                innerBar.color = Color.red;
+            }
+            canvas = innerBar.gameObject.transform.parent.gameObject;
+            canvas.transform.rotation = Camera.main.transform.rotation;
+        }
+        
+        
+
     }
 
     public void LoadFirstLevel()
@@ -36,9 +63,13 @@ public class UIManager : MonoBehaviour
     {
         if(scene.buildIndex == 1)
         {
-            GameObject[] quitButton = GameObject.FindGameObjectsWithTag("QuitButton");
-            Button btn = quitButton[0].GetComponent<Button>();
-            btn.onClick.AddListener(QuitGame);
+            
+            Button quitButton = GameObject.FindGameObjectWithTag("QuitButton").GetComponent<Button>();
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            innerBar = GameObject.FindGameObjectWithTag("PlayerHealthBar").GetComponent<Image>();
+            canvas = innerBar.gameObject.transform.parent.gameObject;
+            quitButton.onClick.AddListener(QuitGame);
+            
         }
     }
 }
